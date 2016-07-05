6fb03bdc0b9537e5be2d9cd72830c7d25938b8f5
Kotaro Miura <miura@at-iroha.jp>
2016-07-05 20:00:52 +0900

    Add demo mode

---
+++ webroot/js/demo.js
@@ -0,0 +1,42 @@
+/**
+ * OCoMMS Project
+ *
+ * @author        Kotaro Miura
+ * @copyright     2016 Advanced Institute of Industrial Technology
+ * @link          http://aiit.ac.jp/
+ * @license       http://www.gnu.org/licenses/gpl-3.0.en.html GPL License
+ */
+ 
+$(function (event)
+{
+	/*
+	$('.btn').click(function(event)
+	{
+		event.preventDefault();
+		return false;
+	});
+	*/
+	
+	$('.btn-primary').prop('disabled', true);
+	$('.btn-danger').attr("onclick", 'alert("デモモードの為、削除できません");');
+	
+	$('.btn-primary[value="+ 追加"]').prop('disabled', false);
+	$('.btn-primary[value="追加"]').prop('disabled', false);
+	$('.btn-primary[value="検索"]').prop('disabled', false);
+	$('.btn-add').prop('disabled', false);
+	$('.btn[value="ログイン"]').prop('disabled', false);
+	
+	if(location.href.indexOf('admin') > 0)
+	{
+		$("#UserUsername").val("root");
+		$("#UserPassword").val("pass");
+	}
+	else
+	{
+		var day = ((new Date()).getDay()+1);
+		
+		$("#MemberUsername").val("20001");
+		$("#MemberPassword").val("pass");
+	}
+});
+
