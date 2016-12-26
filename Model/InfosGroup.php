5b856612146d72749db54e4b9d467c3e34f3d214
Kotaro Miura <miura@irohasoft.jp>
2016-12-26 13:30:41 +0900

    Change copyright

--- Model/InfosGroup.php
+++ Model/InfosGroup.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * iroha Board Project
+ * OAMMS Project
  *
  * @author        Kotaro Miura
- * @copyright     2015-2016 iroha Soft, Inc. (http://irohasoft.jp)
- * @link          http://irohaboard.irohasoft.jp
+ * @copyright     2016 Advanced Institute of Industrial Technology
+ * @link          http://aiit.ac.jp/
  * @license       http://www.gnu.org/licenses/gpl-3.0.en.html GPL License
  */
 
